7aefbb4f00a3985e38e6be750d7df13dddb06e3e
HelixOS <vitaliy@thecto.co.il>
2017-03-01 13:08:04 +0200

    Update miscdev.c

--- google/msm/fs/ecryptfs/miscdev.c
+++ google/msm/fs/ecryptfs/miscdev.c
@@ -3,6 +3,7 @@
  *
  * Copyright (C) 2008 International Business Machines Corp.
  *   Author(s): Michael A. Halcrow <mhalcrow@us.ibm.com>
+ * Modifications Copyright (C) 2011-2017 TheCTO Technological Entrepreneurship & Consulting LTD
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License version
